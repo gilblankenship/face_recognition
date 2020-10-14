ca164bb6bcf2fc6b6aee8168775b2b23d168c9c3
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-14 17:11:44 -0400

    Added variable cameraNumber Added variable frameSize

--- examples/find_faces_in_picture.py
+++ examples/find_faces_in_picture.py
@@ -2,7 +2,7 @@ from PIL import Image
 import face_recognition
 
 # Load the jpg file into a numpy array
-image = face_recognition.load_image_file("two_people.jpg")
+image = face_recognition.load_image_file("Biden, Trump, and Obama.jpg")
 
 # Find all the faces in the image using the default HOG-based model.
 # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
