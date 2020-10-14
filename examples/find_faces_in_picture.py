8ab1e0a2d098227055f18c579eaa52d47792a1e6
gilblankenship <57296748+gilblankenship@gmail.com>
2020-10-14 10:26:51 -0400

    Added variable cameraNumber Added variable frameSize

--- examples/find_faces_in_picture.py
+++ examples/find_faces_in_picture.py
@@ -17,8 +17,7 @@ for face_location in face_locations:
     top, right, bottom, left = face_location
     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 
-
     # You can access the actual face itself like this:
-    # face_image = image[top:bottom, left:right]
-    # pil_image = Image.fromarray(face_image)
-    # pil_image.show()
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
